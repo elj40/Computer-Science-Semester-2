f3649ee36a4d28a7c8ec4990a9a982156ac15677
elj40 <elaijoubert@gmail.com>
2024-09-21 17:52:35 +0200

    Drawing board correclty

--- main.c
+++ main.c
@@ -31,12 +31,14 @@
 #include <stdio.h>
 
 void display_map(char *map, int s);
+void print_horz_border(int s);
+void print_horz_row(char *map, int r, int s);
 void clear_map(char *map, int s);
 
 int main() {
 	printf("Hello world\n");
 
-	int map_size = 4;
+	int map_size = 10;
 	char map_chars[map_size][map_size];
 
 	clear_map(&map_chars[0][0], map_size);
@@ -48,7 +50,7 @@ void clear_map(char *map, int s) {
 	for (int i = 0 ; i < s; i++) {
 		for (int j = 0 ; j < s; j++) {
 			int index = i*s + j;
-			*(map + index) = 'a';
+			*(map + index) = ' ';
 		}
 	}
 }
@@ -79,18 +81,49 @@ void display_map(char *map, int s) {
 	   /* +---+---+---+---+---+---+---+---+---+---+ */
 	/* 000|   |   |   |   |   |   |   |   |   |   | */
 	   /* +---+---+---+---+---+---+---+---+---+---+ */
-	// TODO: implement it the proper way
-	/* printf("   "); //Empty 3 spaces for top left corner */
 
-	printf("Displaying map\n\n");
 
+	//Print first line that displays columns
+	printf("   "); //3 empty spaces for top left corner
 	for (int i = 0 ; i < s; i++) {
-		for (int j = 0 ; j < s; j++) {
-			int index = i*s + j;
-			/* printf("%c", *(*(map+i)+j)); */
-			printf("%c", map[index]);
+		printf("%03d ", i);
+	}
+	printf("\n");
+	
 
-		}
+	for (int i = s-1; i >= 0; i--) {
+		print_horz_border(s);
+		printf("\n");
+		print_horz_row(map, i,s);
 		printf("\n");
 	}
+	
+	//Print horizontal border once before
+	print_horz_border(s);
+	printf("\n");
+
+}
+
+void print_horz_border(int s) {
+	//Prints the a horizontal border between cells
+	//Input: size of map
+	//Output: none
+	printf("   "); //3 left padding
+	for (int i = 0; i < s; i++) {
+		printf("+---");
+	}
+	printf("+");
+}
+
+void print_horz_row(char *map, int r, int s) {
+	//Prints the a horizontal border between cells
+	//Input: 
+	//	r: current row
+	//	s: size of map
+	//Output: none
+	printf("%03d", r); 
+	for (int i = 0; i < s; i++) {
+		printf("| %c ", *(map + r*s + i));
+	}
+	printf("|");
 }
